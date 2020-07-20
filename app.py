3ea7752f1484c1bf0caebfa35bf4a21449c886a8
Piero <madarp@pobox.com>
2020-07-20 12:55:43 -0400

    Update starter code to use SlackEventAdapter

--- app.py
+++ app.py
@@ -1,5 +1,19 @@
+#!/usr/bin/env python3
 """
 Slackbot that uses Flask and Slack Events API under the hood
 """
+import os
+from flask import Flask
+from slackeventsapi import SlackEventAdapter
 
-# Add your Flask app code here
+__author__ = '???'
+
+# Create Flask app & events adapter
+app = Flask(__name__)
+slack_events_adapter = SlackEventAdapter(
+    os.environ['SLACK_SIGNING_SECRET'],
+    "/slack/events",
+    app
+    )
+
+# Add remainder of your Flask App code here
